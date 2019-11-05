5464b43551c4ca0f118348795623f98c726873f2
Rafael Madureira dos Santos <rafael.madureira@luizalabs.com>
2019-11-05 18:58:33 -0300

    Add GLEW and Glad as dependencies

--- premake5.lua
+++ premake5.lua
@@ -1,22 +1,58 @@
-workspace "morpheus"
+workspace "Morpheus"
    architecture "x64"
-   startproject "morpheus"
+   startproject "Morpheus"
    configurations { "Debug", "Release" }
    platforms { "x64", "x32" }
-
+   
 outputdir = "%{cfg.system}/%{cfg.buildcfg}/%{cfg.platform}"
+   
+IncludeDir = {}
+IncludeDir["GLFW"] = "Libraries/GLFW/include"
+IncludeDir["Glad"] = "Libraries/Glad/include"
 
-project "morpheus"
+LibrariesDir = {}
+LibrariesDir["GLFW"] = "Libraries/GLFW/lib/%{cfg.platform}"
+
+group "Dependencies"
+	include "Libraries/Glad"
+
+group ""
+
+project "Morpheus"
+   location "Generated"
    kind "ConsoleApp"
    language "C++"
-   cppdialect "C++17"
-   objdir("build/" .. outputdir)
-   targetdir("bin/" .. outputdir)
+   cppdialect "C++11"
+   objdir("Build/" .. outputdir)
+   targetdir("Dist/" .. outputdir)
 
    files {
-      "%{prj.name}/src/**.h",
-      "%{prj.name}/src/**.cpp"
+      "%{prj.name}/Source/**.h",
+      "%{prj.name}/Source/**.cpp"
    }
+
+   includedirs
+	{
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.Glad}"
+   }
+
+   libdirs
+   {
+      "%{LibrariesDir.GLFW}"
+   }
+
+   links 
+	{ 
+      "glfw3",
+      "Glad"
+	}
+	
+	filter { "system:windows" }
+      links { "OpenGL32" }
+   
+   filter { "system:not windows" }
+      links { "GL" }
 
    filter "system:windows"
 		systemversion "latest"
@@ -28,3 +64,20 @@ project "morpheus"
    filter "configurations:Release"
       defines { "NDEBUG" }
       optimize "On"
+
+
+-- Clean Function --
+newaction {
+   trigger = "clean",
+   description = "clean the build",
+   execute = function()
+      print("Cleanning the build...")
+      os.rmdir("./.vs")
+      os.rmdir("./Build")
+      os.rmdir("./Dist")
+      os.rmdir("./Generated")
+      os.remove("./Morpheus.sln")
+      os.remove("./Makefile")
+      print("Done.")
+   end
+}
